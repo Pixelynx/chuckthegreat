7d9c0607f67b53963cb3ef85655f0c2224305d21
pixelynx <tjstevens1992@gmail.com>
2019-04-02 11:03:46 -0400

    there are changes apparently

--- frontend/src/frontpage/under_construction.jsx
+++ frontend/src/frontpage/under_construction.jsx
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import TwitterLogo from './imgs/twitter_logo.png';
+
+import './style.css';
 
 export const UnderConstruction = () => {
   return(
@@ -7,7 +11,11 @@ export const UnderConstruction = () => {
       <h1 className='under_construction'>Under Contruction</h1>
       <p className='sites_to_follow'>Follow Me on All My Social Media</p>
       <div className='player_containter'></div>
-      <div className='links'></div>
+      <div className='links'>
+        <a href='https://www.twitter.com'><img className='twitter_logo'></img></a>
+        <a href='https://www.instagram.com'><img className='ig_logo'></img></a>
+        <a href='https://www.apple.com'><img className='apple_logo'></img></a>
+      </div>
     </div>
     </>
   )
