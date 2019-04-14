1392cad11403b1c5cfcad7ef9e54fca1c2915bac
pixelynx <tjstevens1992@gmail.com>
2019-04-14 16:17:11 -0400

    menu hidden

--- frontend/src/frontpage/index/navigation.jsx
+++ frontend/src/frontpage/index/navigation.jsx
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import '../styles/navBar.css';
 
 class Navigation extends Component {
   render() {
     return(
-      <div>Nav</div>
+      <>
+          <div className='nav_container'>
+          <div className='menu_button'>
+          </div>
+            <div className='nav_bar'>
+              <ul className='nav_bar_ul'>
+                <li className='nav_link'>Home</li>
+                <li className='nav_link'>About</li>
+                <li className='nav_link'>Music</li>
+                <li className='nav_link'>Gallery</li>
+                <li className='nav_link'>Perform</li>
+                <li className='nav_link'>Contact</li>
+              </ul>
+            </div>
+          </div>
+        </>
     )
   }
 }
