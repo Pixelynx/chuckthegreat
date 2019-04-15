52ad778c6593dc2babb0a1c39167a6fd8e7d9a5f
pixelynx <tjstevens1992@gmail.com>
2019-04-14 23:32:55 -0400

    nav animation

--- frontend/src/frontpage/index/navigation.jsx
+++ frontend/src/frontpage/index/navigation.jsx
@@ -1,24 +1,43 @@
 import React, { Component } from 'react';
+import { Link, Route } from 'react-router-dom';
+import LandingPage from '../landing/landingPage.jsx';
 import '../styles/navBar.css';
 
 class Navigation extends Component {
+  state = {
+    visibility: false
+  }
+
+  handleMenuVisibility = (e) => {
+    const currentState = this.state.visibility;
+    this.setState({ visibility: !currentState })
+  }
+
+
   render() {
+    console.log(this.state)
+    const { visibility } = this.state;
+
+
     return(
       <>
-          <div className='nav_container'>
-          <div className='menu_button'>
-          </div>
-            <div className='nav_bar'>
+          <div id='nav_container' onClick={this.handleMenuVisibility} className={visibility}>
+          <div className='menu_button' onClick={this.handleMenuVisibility}>click</div>
+            <div id='nav_bar' className={visibility ? 'show' : 'hide'}>
               <ul className='nav_bar_ul'>
-                <li className='nav_link'>Home</li>
-                <li className='nav_link'>About</li>
-                <li className='nav_link'>Music</li>
-                <li className='nav_link'>Gallery</li>
-                <li className='nav_link'>Perform</li>
-                <li className='nav_link'>Contact</li>
+                <li className='nav_link'><Link exact path to='/home'>Home</Link></li>
+                <li className='nav_link'><Link path to='/about'>About</Link></li>
+                <li className='nav_link'><Link path to='/music'>Music</Link></li>
+                <li className='nav_link'><Link path to='/gallery'>Gallery</Link></li>
+                <li className='nav_link'><Link path to='/perform'>Perform</Link></li>
+                <li className='nav_link'><Link path to='/contact'>Contact</Link></li>
               </ul>
             </div>
+
           </div>
+          <Route exact path='/home' component={ LandingPage } />
+
+
         </>
     )
   }
---
+++ frontend/src/frontpage/landing/landingPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LandingPage extends Component {
+  render() {
+    return(
+      <>
+        <h1 className='chuckthegreat'>CHUCKTHEGREAT</h1>
+      </>
+    )
+  }
+}
+
+export default LandingPage;
